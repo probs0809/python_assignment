5244d6200e0fa1ef09a6e377ae73057283bc13e5
probs0809 <prabodhmayekar@gmail.com>
2020-04-06 16:47:51 +0530

    initial commit

---
+++ get_data.py
@@ -0,0 +1,23 @@
+import csv, json
+import pandas as pd
+import requests
+
+json_data_url = requests.get('https://api.covid19india.org/raw_data.json')
+json_file = open('/Users/prabodhmayekar/Documents/rawjson.json','w')
+json_file.write(str(json_data_url.text))
+json_file.close()
+
+fileInput = '/Users/prabodhmayekar/Documents/rawjson.json'
+fileOutput = '/Users/prabodhmayekar/Documents/raw2.csv'
+inputFile = open(fileInput) 
+outputFile = open(fileOutput, 'w') 
+data = json.load(inputFile)
+inputFile.close()
+output = csv.writer(outputFile)
+output.writerow(data['raw_data'][0].keys())
+for row in data['raw_data']:
+    output.writerow(row.values())
+    
+pd.read_csv(fileOutput).to_excel('/Users/prabodhmayekar/Documents/covid_19Data.xlsx')
+print('Completed...')
+
