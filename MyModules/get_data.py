7205b78b08075e9293f5834551198cd5d18854f5
probs0809 <prabodhmayekar@gmail.com>
2020-04-20 15:17:19 +0530

    Update m.1

--- MyModules/get_data.py
+++ MyModules/get_data.py
@@ -3,27 +3,21 @@ from pandas.io.json import json_normalize
 import requests
 import json
 from pathlib import Path
-RAW_DATA = Path('./covid_19Data_raw.xlsx')
-CLEAN_DATA = Path('./covid_19Data_clean.xlsx')
+RAW_DATA = Path('./Data/covid_19Data_raw.xlsx')
+CLEAN_DATA = Path('./Data/covid_19Data_clean.xlsx')
 
 def get_data(api = 'raw_data',key = 'raw_data'):
-    print("Fetching data...")
     json_data_url = requests.get('https://api.covid19india.org/'+api+'.json')
-    print("Fetching data completed...")
-    print("Creating excel sheet...")
     js = json.loads(json_data_url.text)
     df = json_normalize(js[key])
     df.to_excel(RAW_DATA)
-    print("Creating excel sheet completed...")
     return True
 
 def clean_data():
-    print("Cleaning data...")
     df = pd.read_excel(RAW_DATA)
     df['agebracket'].fillna(-1,inplace = True)
     df['gender'].fillna('UK',inplace = True)
     df['statecode'].fillna('UK',inplace=True)
     df = df[['agebracket','gender','statecode','currentstatus','detectedstate']][df['detectedstate'].notna()]  
     df.to_excel(CLEAN_DATA)  
-    print("Cleaning data completed...")
     return True
--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import get_data
-import generate_graph as gg
+import MyModules.get_data as gd
+import MyModules.generate_graph as gg
 from pathlib import Path
 
 # Uncomment the following lines of code while executing for the first time
-get_data.get_data()
-get_data.clean_data() 
-df = pd.read_excel(get_data.CLEAN_DATA)
-print("Generating graphs...")
-gg.generate_male_female_statewise_graph(df,Path('./MaleFemaleStatewise.png'))
-gg.generate_statewise_death_recovery_graph(df,Path('./statewiseStatusGraph.png'))
-gg.generate_death_recovery_total_plot(df,Path('./deathandrecoveryandtotal.png'))
-print("Graphs generated in local files")
+gd.get_data()
+gd.clean_data() 
+df = pd.read_excel(gd.CLEAN_DATA)
+
+gg.generate_male_female_statewise_graph(df,Path('./Visualisations/MaleFemaleStatewise.png'))
+gg.generate_statewise_death_recovery_graph(df,Path('./Visualisations/statewiseStatusGraph.png'))
+gg.generate_death_recovery_total_plot(df,Path('./Visualisations/deathandrecoveryandtotal.png'))
 print("FINISH")
